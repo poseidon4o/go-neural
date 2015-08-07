aef0692524737da5555ffb09bf4ea060b3b7883f
poseidon4o <poseidon4o.random@gmail.com>
2015-08-07 13:47:22 +0300

    Add flappy step

--- src/problems/flappy.go
+++ src/problems/flappy.go
@@ -8,21 +8,28 @@ type Vector struct {
 	x, y float64
 }
 
-const ZeroVector Vector = &Vector{
-	x: 0.0,
-	y: 0.0,
-}
-
-func (v *Vector) Add(o *Vector) *Vector {
+func (v Vector) Add(o *Vector) *Vector {
 	v.x += o.x
 	v.y += o.y
-	return v
+	return &v
 }
 
-func (v *Vector) Neg() *Vector {
+func (v Vector) Neg() *Vector {
 	v.x = -v.x
 	v.y = -v.y
-	return v
+	return &v
+}
+
+func (v Vector) Mul(o *Vector) *Vector {
+	v.x *= o.x
+	v.y *= o.y
+	return &v
+}
+
+func (v Vector) Scale(scalar float64) *Vector {
+	v.x *= scalar
+	v.y *= scalar
+	return &v
 }
 
 func NewVector(x, y float64) *Vector {
@@ -37,6 +44,11 @@ type Bird struct {
 }
 
 const pylonSpacing int = 50
+const G_CONST float64 = 9.8
+const G_FORCE Vector = Vector{
+	x: 0,
+	y: G_CONST,
+}
 
 type Level struct {
 	size   Vector
@@ -60,5 +72,34 @@ func NewLevel(w, h int) *Level {
 }
 
 func (l *Level) AddBirds(count int) {
+	for c := 0; c < count; ++c {
+		vpos := int(neural.RandMax(l.size.y / 5) + l.size.y / 10)
 
+		l.birds = append(l.birds, Bird{
+			pos: *NewVector(1, vpos),
+			vel: *NewVector(0, 0),
+		})
+	}
+}
+
+func (l *Level) GetBirds() []Bird {
+	return l.birds
+}
+
+func (l *Level) GetPylons() []Vector {
+	return l.pylons
+}
+
+func (l *Level) GetSize() Vector {
+	return l.size
+}
+
+func (l *Level) Step(dt float64) {
+	for c := range l.birds {
+		// position += timestep * (velocity + timestep * acceleration / 2);
+		birds[c].pos = birds[c].pos.add(G_FORCE.scale(dt / 2).add(l.birds[c].vel).scale(dt))
+
+		// velocity += timestep * acceleration;
+		birds[c].vel = birds[c].vel.add(G_FORCE.scale(dt))
+	}
 }
